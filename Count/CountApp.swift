d837e84832e97b365f4560586b6c1e43145e8bb6
Aoi-Kobayashi <oosabaaoi@gmail.com>
2025-07-20 13:46:01 +0900

    Initial Commit

---
+++ Count/CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountApp.swift
+//  Count
+//
+//  Created by AoiKobayashi on 2025/07/20.
+//
+
+import SwiftUI
+
+@main
+struct CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
