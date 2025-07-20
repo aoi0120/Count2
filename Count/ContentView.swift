75f5cca9e7b9ca62812c38207131e04401f67b7d
Aoi-Kobayashi <oosabaaoi@gmail.com>
2025-07-20 14:35:32 +0900

    init setup

--- Count/ContentView.swift
+++ Count/ContentView.swift
@@ -8,12 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
+    @State var result = ""
+    
+    func judgeResult(count: Int) {
+        if count >= 10 {
+            result = "プラスの勝ち！"
+        } else if count >= 7 {
+            result = "プラスが勝ちそう！"
+        } else if count <= -10 {
+            result = "マイナスの勝ち！"
+        } else if count <= -7 {
+            result = "マイナスが勝ちそう！"
+        } else {
+            result = ""
+        }
+    }
+    
+    func judgeCorlor(count: Int) -> Color {
+        return count < 0 ? .blue : (count > 0 ? .red : .black)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Text("\(result)")
+                .frame(width: 300, height: 100)
+                .font(.system(size: 32))
+            Text("\(count)")
+                .frame(width: 300, height: 100)
+                .font(.system(size: 48))
+                .foregroundColor(judgeCorlor(count: count))
+            
+            HStack {
+                Button(action: {
+                    count += 1;
+                    judgeResult(count: count)
+                },label: {
+                    Image(systemName: "plus")
+                })
+                .font(.system(size: 48))
+                .foregroundColor(.white)
+                .frame(width: 100, height: 100)
+                .background(.red)
+                
+                Button (action: {
+                    count -= 1;
+                    judgeResult(count: count)
+                }, label: {
+                    Image(systemName: "minus")
+                })
+                .font(.system(size: 48))
+                .frame(width: 100, height: 100)
+                .foregroundColor(.white)
+                .background(.blue)
+                
+            }
+            .padding()
+            
         }
         .padding()
     }
